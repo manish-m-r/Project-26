12a7f4899cbd5b578d9634c78073bd134e0d1b58
LAPTOP-C8989RFI\yashw <yashwanthd.15@gmail.com>
2021-11-12 12:13:58 -0700

    FAQ web page is designed

--- HtmlAndCss/FAQmain.js
+++ HtmlAndCss/FAQmain.js
@@ -13,11 +13,4 @@ for (i = 0; i < faq.length; i++) {
             body.style.display = "block";
         }
     });
-    function myFunction(imgs) {
-  var expandImg = document.getElementById("expandedImg");
-  var imgText = document.getElementById("imgtext");
-  expandImg.src = imgs.src;
-  imgText.innerHTML = imgs.alt;
-  expandImg.parentElement.style.display = "block";
-}
 }
