078d2f8aafe348869082c959d901919444d06785
Manish <manish.m.r.96@gmail.com>
2021-11-12 00:49:02 -0700

    Total file chages

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const path= require('path')
 const connectDB = require('./config/db');
 var cors = require('cors');
-
 const app = express();
 
-app.get('/',(req,res) => res.send('API Running'));
+//app.get('/',(req,res) => res.send('API Running'));
 connectDB();
 
 app.use(cors());
@@ -12,6 +12,27 @@ app.use(express.json());
 app.use('/api/admin', require('./routes/api/admin'));
 app.use('/api/team', require('./routes/api/team'));
 app.use('/api/referee', require('./routes/api/referee'));
+
+
+// GET route '/'
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/HtmlAndCss/index.html')); 
+})
+
+app.get('/referee.html', function (req, res) {
+   res.sendFile(path.join(__dirname + '/HtmlAndCss/referee.html'));
+})
+
+app.get('/refereeSignUP.html', function (req, res) {
+   res.sendFile(path.join(__dirname + '/HtmlAndCss/refereeSignUP.html'));
+})
+
+
+app.get('/admin.html', function (req, res) {
+   res.sendFile(path.join(__dirname + '/HtmlAndCss/admin.html')); 
+})
+
+
 
 
 const PORT = process.env.PORT || 5000;
